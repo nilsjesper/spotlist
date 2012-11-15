c34c0d9cd3b2f596bd8bba8e82e1e8e2bf3798a9
Martin Olson <molson@hugeinc.com>
2012-11-14 22:36:05 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spotlist::Application.config.secret_token = '30f32fa19cab0fec21df7645186ccfeda6bfcf3ed93dc8c86d4fbedbc6f6cd3d08c52ab4553b3e50897ab1085a911e98e80d147143a09c1818327b7ee8f448f3'
